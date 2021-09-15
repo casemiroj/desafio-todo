9b01e104c5ca697e18e15b2a7cf444dad6470068
João Casemiro <jv.casemiro@icloud.com>
2021-09-15 13:17:41 -0300

    Finalizando desafio

--- src/index.js
+++ src/index.js
@@ -69,15 +69,50 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+  const { title, deadline } = request.body;
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo) {
+    return response.status(404).json({error: 'Todo not found!'});
+  }
+
+  todo.title = title;
+  todo.deadline = new Date(deadline);
+
+  return response.json(todo);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo) {
+    return response.status(404).json({error: 'Todo not found!'});
+  }
+
+  todo.done = true;
+
+  return response.json(todo);
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.findIndex(todo => todo.id === id);
+
+  if(todo < 0) {
+    return response.status(404).json({error: 'Todo not found!'})
+  }
+
+  user.todos.splice(todo, 1)
+
+  return response.status(204).send();
 });
 
 module.exports = app;
